5ad6ea3041fc5685d21fc2e0ee30d914f8336aa8
Rony <ronyalexis77@gmail.com>
2021-04-20 09:18:32 -0600

    Integracion paquete qr_scan

--- lib/pages/widgets/scan_button.dart
+++ lib/pages/widgets/scan_button.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 10,
-      onPressed: (){},
+      onPressed: () async {
+        // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        //     '#3d8bef', 'Cancelar', false, ScanMode.QR);
+
+        final barcodeScanRes = 'https://fernando-herrera.com';
+        print("El resultado:" + barcodeScanRes);
+      },
       child: Icon(Icons.filter_center_focus),
     );
   }
